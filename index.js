8764d9cea0f6d0cd302da692a66fcbe3fefa0a5b
Geet-manik <106328996+Geet-manik@users.noreply.github.com>
2022-07-07 02:51:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log("this is tut 1");
+let a=document.getElementById('button1');
+a.addEventListener('click',function(e)
+{
+let b=document.getElementById('lorem1');
+b.innerText='You Clicked button 1';
+
+});
+let c=document.getElementById('button2');
+c.addEventListener('click',function(e)
+{
+let d=document.getElementById('lorem1');
+d.innerText='You Clicked button 2';
+
+});
+
